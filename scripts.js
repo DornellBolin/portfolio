e43d10c117cfe8aec1fd6a9cd63d135e142a4be2
Dornell Bolin <dornellbolins@gmail.com>
2017-07-20 11:30:30 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,19 +1,33 @@
-var projects =[
+var projects = [
     {
-        "name": "Shop project"  
-       
+        "name": "Shop project"
+
     }
 ];
-
+ 
+ var backgroundImages = [
+        '1.gif',
+        '2.gif',
+        '3.gif'
+     ];
+     var i = 0;
+      setInterval(function() {
+         $("body").css('background-image', 'url("portfolio/background images' + backgroundImages[i] + '")');
+         i = i + 1;
+         if (i == 3) {
+             i = 0;
+        }
+    }, 9000);
+  
 
 $(document).ready(function () {
 
-$.each(projects, function(index, project){
-    console.log(project);
-    $('#projects').append('<a href="#"'+ project.name + '</a>' );
-});
-    
-$('#ContactForm').on('submit', submitContactForm);
+    $.each(projects, function (index, project) {
+        console.log(project);
+        $('#projects').append('<a href="#"' + project.name + '</a>');
+    });
+
+    $('#ContactForm').on('submit', submitContactForm);
 
 });
 function submitContactForm() {
@@ -28,3 +42,6 @@ function submitContactForm() {
         //  alert('Thank you for submitting the form');
     }
 }
+
+
+  
