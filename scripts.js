02eea1573596172778046dabea60eb9b2b4b478c
Dornell Bolin <dornellbolins@gmail.com>
2017-07-21 12:48:49 -0500

    updates

--- scripts.js
+++ scripts.js
@@ -6,10 +6,23 @@ var projects = [
 ];
 
 
-
  
 
 $(document).ready(function () {
+    var backgroundImages = [
+        '1.gif',
+        '2.gif',
+        '3.gif'
+    ];
+    var i = 0;
+    setInterval(function () {
+        console.log(backgroundImages[i]);
+        $("body").css('background-image', 'url("' + backgroundImages[i] + '")');
+        i = i + 1;
+        if (i == 3) {
+            i = 0;
+        } 
+    }, 5000);
 
     $.each(projects, function (index, project) {
         console.log(project);
